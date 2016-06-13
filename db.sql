3c48e4941eda729ce5a2b4e79ae3e904fae61102
Mitch-C <mitchell.coad@hotmail.co.nz>
2016-06-14 11:44:28 +1200

    database file

---
+++ db.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE Users (Uid SERIAL, Username VARCHAR(50),
+RealName VARCHAR(100), Password VARCHAR(50), contact character varying(100));
+
+
+
+
+
+INSERT INTO Users (Username,Realname,Password,manager) VALUES
+('admin','Sally Smith','admin','t');
+
+
+
+
