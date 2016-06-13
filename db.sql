291ee00e28a72a9200bada398ab4c9f06fafe0fb
Mitch-C <mitchell.coad@hotmail.co.nz>
2016-06-14 11:53:51 +1200

    fix table error

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
-﻿CREATE TABLE Users (Uid SERIAL, Username VARCHAR(50),
-RealName VARCHAR(100), Password VARCHAR(50), contact character varying(100));
+CREATE TABLE Users (Uid SERIAL, Username VARCHAR(50),
+RealName VARCHAR(100), Password VARCHAR(50), manager character varying(100));
 
 
 
@@ -7,7 +7,6 @@ RealName VARCHAR(100), Password VARCHAR(50), contact character varying(100));
 
 INSERT INTO Users (Username,Realname,Password,manager) VALUES
 ('admin','Sally Smith','admin','t');
-
 
 
 
