5cc61041d81b682a77c74e37fe5b5cf8b71800e1
Mitch-C <mitchell.coad@hotmail.co.nz>
2016-06-17 13:34:16 +1200

    add links

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var queries = require('../queries');
 //var database = "postgres://depot:5432/Swen301";
 
 //At Home
-var pg = require('pg').native;
+var pg = require('pg');
 //Daniel
 //var database = "postgres://postgres:w2sybb57@localhost:5432/swen301";
 //Pas
@@ -16,15 +16,15 @@ var pg = require('pg').native;
 //var database = "postgres://postgres:123456@localhost:5432/postgres";
 //var pg = require('pg');
 var database = "postgres://postgres:postgres@localhost:5432/swen301";
-
+var items = [];
 pg.connect(database, function (err) {
   if (err) {
     console.error('Could not connect to the database.');
     console.error(err);
     return;
-  }
+}
 
-  console.log('Connected to database.');
+console.log('Connected to database.');
 });
 
 var signedInUserRealname = '';
@@ -38,65 +38,65 @@ router.get('/', function(req, res) {
 
 /* GET home page. */
 router.get('/mainPage', function(req, res) {
-  res.render('mainPage', { message: req.query.message, redirect: req.query.redirect, id: signedInUserUID, manager: manager });
+  res.render('mainPage', { message: req.query.message, redirect: req.query.redirect, id: signedInUserUID, manager: manager,items:items });
 });
 
 router.get('/checkFigure', function(req, res){
   var routes = 0;
   var revenue = 0;
   var expenditure = 0;
-    pg.connect(database, function (err, client, done) {
-        if (err) {
-            console.error('Could not connect to the database.');
-            console.error(err);
+  pg.connect(database, function (err, client, done) {
+    if (err) {
+        console.error('Could not connect to the database.');
+        console.error(err);
+        return;
+    }
+
+    var query = "SELECT COUNT(*) FROM Routes" ;
+    client.query(query, function (error, result) {
+        done();
+        if (error) {
+            console.error('Failed to execute query.');
+            console.error(error);
             return;
         }
-
-        var query = "SELECT COUNT(*) FROM Routes" ;
-        client.query(query, function (error, result) {
-            done();
-            if (error) {
-                console.error('Failed to execute query.');
-                console.error(error);
-                return;
-            }
-            else{
-              routes = result.rows[0].count;
-            }    
-        });
-
-        var query = "SELECT * FROM Expenditure" ;
-        client.query(query, function (error, result) {
-            done();
-            if (error) {
-                console.error('Failed to execute query.');
-                console.error(error);
-                return;
-            }
-            else{
-              expenditure = result.rows[0].expenditure;
-            }         
-        });
-
-        var query = "SELECT * FROM Revenue" ;
-          client.query(query, function (error, result) {
-            done();
-            if (error) {
-                console.error('Failed to execute query.');
-                console.error(error);
-                return;
-            }
-            else{
-              revenue = result.rows[0].revenue;
-             
-               res.render('checkFigure', {  numberofroutes:routes, totalRev:revenue,totalExpenditure:expenditure});
-
-            }
-            });
-          
-         
-    }); 
+        else{
+          routes = result.rows[0].count;
+      }    
   });
+
+    var query = "SELECT * FROM Expenditure" ;
+    client.query(query, function (error, result) {
+        done();
+        if (error) {
+            console.error('Failed to execute query.');
+            console.error(error);
+            return;
+        }
+        else{
+          expenditure = result.rows[0].expenditure;
+      }         
+  });
+
+    var query = "SELECT * FROM Revenue" ;
+    client.query(query, function (error, result) {
+        done();
+        if (error) {
+            console.error('Failed to execute query.');
+            console.error(error);
+            return;
+        }
+        else{
+          revenue = result.rows[0].revenue;
+
+          res.render('checkFigure', {  numberofroutes:routes, totalRev:revenue,totalExpenditure:expenditure});
+
+      }
+  });
+
+
+}); 
+});
 
 /* GET routes page. */
 router.get('/editAccount', function(req, res) {
@@ -112,12 +112,54 @@ router.get('/editAccount', function(req, res) {
             done();
             if (error) {
                 console.error('Failed to execute query.');
-                console.error(error);
+                console.error(error);                  
                 return;
             }
             res.render('editAccount', { title: websiteName, list: result.rows, message: req.query.message, signedInUser: signedInUser, redirect: req.query.redirect, id: signedInUserUID,manager: manager });
         });
     });
+});
+
+/* GET routes page. */
+router.get('/search', function(req, res) {
+   
+var items =[];
+pg.connect(database, function (err, client, done) {
+    if (err) {
+        console.error('Could not connect to the database.');
+        console.error(err);
+        return;
+    }
+
+    var query = "SELECT * FROM Routes" ;
+    client.query(query, function (error, result) {
+        done();
+        if (error) {
+            console.error('Failed to execute query.');
+            console.error(error);               
+            return;
+        }
+
+        for(var i=0; i<result.rows.length; i++){
+            var item = {
+                addressorigin: result.rows[i].addressorigin,
+                suberborigin: result.rows[i].suberborigin,
+                regionorigin: result.rows[i].regionorigin,
+                addressdes: result.rows[i].addressdes,
+                suburbdes: result.rows[i].suburbdes,
+                regiondes: result.rows[i].regiondes,
+                countrydes: result.rows[i].countrydes,
+                priority: result.rows[i].priority,
+
+            };
+            items.push(item);
+        }
+        console.error(items); 
+        console.log(item.regiondes);
+        res.render('mainPage', { message: req.query.message, redirect: req.query.redirect, id: signedInUserUID, manager: manager,items:items});
+
+    });
+});
 });
 
 router.get('/doDeleteAccount', function(req, res) {
