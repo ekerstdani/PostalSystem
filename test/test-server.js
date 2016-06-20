0b309d54e7302437a5c881efdfb583df2a7d2f3d
Blake McCawe <blake.mccawe@gmail.com>
2016-06-21 00:44:38 +1200

    like this.

--- test/test-server.js
+++ test/test-server.js
@@ -1,76 +1,20 @@
 var chai = require('chai');
-var chaiHttp = require('chai-http');
 var queries = require('../queries');
 var should = chai.should();
 var assert = chai.assert;
-chai.use(chaiHttp);
-var pg = require('pg');
-var database = "postgres://postgres:postgres@localhost:5432/swen301";
 
+describe('routes database queries', function() {
 
+ it('should list all routes', function() {
+    queries.getAllRoutes(function(err, result){
+      assert(result.length > 0);
+    });
+ });
 
-describe('database queries', function() {
-  //it('check user is logged on');
-  it('check mocha is working', function() {
-    var arr = [];
-    assert.equal(arr.length, 0);
-   });
-  it('count routes', function() {
-    var query = "SELECT COUNT(*) FROM Routes";
-  	 pg.connect(query, function (error, result) {
-        done();
-        if (error) {
-           return
-        }
-        else{
-          assert.equal(result.length, 2);
-      }   
-    });    
-   });
-  it('check user', function() {
-    pg.connect(database, function (err, client, done) {
-    if (err) {
-     
-      return;
-    }
-    client.query("DELETE FROM Mail WHERE id=" + id + ";", function (error, result) {
-      done();
-      if (error) {       
-        return;
-      }
-       assert.equal(result.length, 4);
+ it('should get a route by id', function(){
+    queries.getRouteById(1, function(err, result){
+      assert(result.id == 1);
     });
-  	});   
-   }); 
-  it('check user', function() {
-    pg.connect(database, function (err, client, done) {
-    if (err) {
-     
-      return;
-    }
-    client.query("DELETE FROM Mail WHERE id=" + id + ";", function (error, result) {
-      done();
-      if (error) {       
-        return;
-      }
-       assert.equal(result.length, 4);
-    });
-  	});   
-   }); 
-  it('add location', function() {
-    pg.connect(database, function (err, client, done) {
-    if (err) {
-     
-      return;
-    }
-    client.query("DELETE FROM Mail WHERE id=" + id + ";", function (error, result) {
-      done();
-      if (error) {       
-        return;
-      }
-       assert.equal(result.length, 4);
-    });
-  	});   
-   }); 
+ });
 
 });
